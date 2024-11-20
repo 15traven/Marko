36dbc66b7dc3e21409386a502e98c2f814c7f8b4
15traven <sikachyna.n@gmail.com>
2024-11-20 10:49:29 +0200

    feat(core/parser): recognize * as italic

--- mk-core/src/parser.rs
+++ mk-core/src/parser.rs
@@ -232,7 +232,7 @@ impl<'a> Iterator for Parser<'a> {
                 continue;
             }
 
-            if let Some(rest) = self.s.strip_prefix('/') {
+            if let Some(rest) = self.s.strip_prefix('*') {
                 self.s = rest;
                 self.start_of_line = false;
                 self.style.italics = !self.style.italics;
